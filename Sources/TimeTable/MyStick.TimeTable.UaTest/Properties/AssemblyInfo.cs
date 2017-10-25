7ae56c58d245feaff641b7db2357660f950f9d0d
wpankratz <wolfgangpankratz@gmail.com>
2017-10-25 17:47:47 +0200

    Refactoring AppVeyor cache fixed

--- Sources/TimeTable/MyStick.TimeTable.UaTest/Properties/AssemblyInfo.cs
+++ Sources/TimeTable/MyStick.TimeTable.UaTest/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
+/*
 // Allgemeine Informationen über eine Assembly werden über die folgenden
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
@@ -34,3 +34,14 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+*/
+
+[assembly: AssemblyTitle("MyStick.TimeTable.UaTest")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("d185ceb2-1718-4089-bd7a-48fb2a7a3f26")]
