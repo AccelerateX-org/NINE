a5338cef221ecf70b7302248f62cebc410e75a58
olavhinz <olav@acceleratex.org>
2023-06-13 19:28:06 +0200

    release 0.8.55

--- Sources/SolutionInfo.cs
+++ Sources/SolutionInfo.cs
@@ -20,6 +20,6 @@ using System.Reflection;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.8.54.0")]
-[assembly: AssemblyFileVersion("0.8.54.0")]
-[assembly: AssemblyInformationalVersion("0.8.54")]
+[assembly: AssemblyVersion("0.8.55.0")]
+[assembly: AssemblyFileVersion("0.8.55.0")]
+[assembly: AssemblyInformationalVersion("0.8.55")]
