597c8d0ee7683e1cf0da00a0d156c0587db8934b
olavhinz <olav@acceleratex.org>
2025-05-09 10:45:39 +0200

    Release 0.17.2 - OIDC

--- Sources/SolutionInfo.cs
+++ Sources/SolutionInfo.cs
@@ -20,6 +20,6 @@ using System.Reflection;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.17.1.0")]
-[assembly: AssemblyFileVersion("0.17.1.0")]
-[assembly: AssemblyInformationalVersion("0.17.1")]
+[assembly: AssemblyVersion("0.17.2.0")]
+[assembly: AssemblyFileVersion("0.17.2.0")]
+[assembly: AssemblyInformationalVersion("0.17.2")]
