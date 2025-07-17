5dc29972e45bc30106dd6540067d93f0b3fa7cf6
olavhinz <olav@acceleratex.org>
2025-07-17 13:37:23 +0200

    next fix config

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -15,10 +15,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://https://acceleratex-org.github.io',
+  url: 'https://acceleratex-org.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/NINE',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
